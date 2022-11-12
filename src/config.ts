32948b1e745dc66188c13d8fd40ac4e98f1c7558
arslanBaris <baris.arslan@diatics.com>
2022-11-12 22:15:41 +0300

    config.ts added

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+const API_URL = 'https://api.spotify.com/v1';
+const AUTH_URL = 'https://accounts.spotify.com/api/token';
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+
+export {
+  API_URL,
+  AUTH_URL,
+  CLIENT_ID,
+  CLIENT_SECRET
+};
