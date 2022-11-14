edefa3277bfefd407752202eaa9fabba950a7e9d
arslanBaris <baris.arslan@diatics.com>
2022-11-14 18:45:19 +0300

    Edit config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 const API_URL = 'https://api.spotify.com/v1';
 const AUTH_URL = 'https://accounts.spotify.com/api/token';
+
+// Set Your configurations (CLIENT_ID and CLIENT_SECRET)
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
